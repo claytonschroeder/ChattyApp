d37e185cdb26d835c714ab07a021a9ce46d1d795
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2016-11-30 23:06:13 +0000

    changes made to App ChatBar and SCSS. text entered in chat bar updates message list

--- boilerplate/src/App.jsx
+++ boilerplate/src/App.jsx
@@ -1,3 +1,6 @@
+//app.jsx is the parent which needs access to messages and the currentUser.
+//to get the data from its "children"  it must import:
+// MessageList (messages) and ChatBar (currentUser).
 import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
@@ -9,46 +12,39 @@ class App extends Component {
     super(props);
     this.state = {
       currentUser: {name: "Bob"},
-      messages: [
-        {
-          id: 0,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 1,
-          username: "Jimmy",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        },
-        {
-          id: 3,
-          username: "Mike",
-          content: "Suh dude"
-        }
-      ]
+      messages: []
     }
   }
-  componentDidMount() {
-    setTimeout(() => {
-      // creating new message
-      const newMessage = {id: 4, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-
-
-      // Update the state of the app component.
-      // set State calls render() in App and all its children
-      this.setState({messages: messages})
-    }, 3000);
+  //concatinates a new message from the input
+  addMessage(username, content) {
+    console.log('app component received username', username, 'and content', content);
+    let newMessage = {username: username, content: content, id: Date.now()};
+    let listWithNewMessage = this.state.messages.concat(newMessage);
+    this.setState({messages: listWithNewMessage});
   }
 
+  // componentDidMount() {
+  //   console.log("componentDidMount <App />");
+  //   setTimeout(() => {
+  //     console.log("Simulating incoming message");
+  //     // Add a new message to the list of messages in the data store
+  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+  //     const messages = this.state.messages.concat(newMessage)
+  //     // Update the state of the app component.
+  //     // Calling setState will trigger a call to render() in App and all child components.
+  //     this.setState({messages: messages})
+  //   }, 3000);
+  // }
+//rednders the message list with the state of messages
   render() {
+    console.log("Rendering <App />");
     return (
       <div>
         <nav>
           <h1>Chatty</h1>
         </nav>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser} />
+        <ChatBar currentUser={this.state.currentUser} newMessage={this.addMessage.bind(this)}/>
       </div>
     );
   }
